179209ea4ca321e548798b577ca1a58cd1886e47
Kazuryto84 <hurtado.diegop@gmail.com>
2023-11-08 18:20:18 -0500

    Nuevos controladores y handlers

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,20 +1,83 @@
+const { Op } = require("sequelize");
 const { User } = require("../db");
+const axios = require("axios");
 
 const createUserDb = async (name, email, phone) => {
    const newUser = await User.create({name, email, phone});
    return newUser;
 };
 
+const getUserDb = async () => {
+    const allUsersDB = await User.findAll();
+    return allUsersDB;
+};
+
+const getUserApi = async () => {
+    const peticion = (await axios("https://jsonplaceholder.typicode.com/users")).data;
+    const apiInfoMap = peticion.map(user => {
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            phone: user.phone,
+        }
+    })
+    return apiInfoMap;
+};
+
+const getAllUsers = async (name) => {
+    const userDB = await getUserDb()
+    const userApi = await getUserApi()
+    const allUsers = [...userDB,...userApi] //todos los Usuarios
+    if(name){
+        let filterUsers = allUsers.filter((user) => 
+          user.name.toLowerCase().includes(name.toLowerCase())
+        );
+    if(filterUsers.length){
+        return filterUsers
+    }
+    } else {
+        return allUsers;
+    }
+};
+
+const getUserById = async (id) => {
+    if (isNaN(id)){
+        const user = await User.findByPk(id);
+        return user;
+    }
+    const user = (await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)).data;
+    return user;
+};
+
+/* busqueda por coincidencia
 const getUserDb = async (name) => {
     if(name){
-        const userByName = await User.findOne({where:{name}})
-    return userByName
+        const userByName = await User.findAll({
+            limit: 3,
+            where:{
+                name: { [Op.like]: "%" + name + "%"},
+            },
+        });
+    return userByName;
     }
     const allUsers = await User.findAll();
     return allUsers;
 };
+*/
+
+/*por orden
+const getUserDb = async () => {
+        const userByName = await User.findAll({
+            order: [["name", "ASC"]]
+        });
+    return userByName;
+    };
+*/    
+
 
 module.exports = {
     createUserDb,
-    getUserDb,
+    getUserById,
+    getAllUsers
 };
--- src/handlers/usersHandlers.js
+++ src/handlers/usersHandlers.js
@@ -1,14 +1,14 @@
-const {getUserDb} = require("../controllers/UserController")
+const {getAllUsers, getUserById} = require("../controllers/UserController")
 
 // --> query user/?name=
 const getUserHandler = async (req, res) => {
  const { name } = req.query;
  try {
     if(name) {
-        const response = await getUserDb(name);
+        const response = await getAllUsers(name);
         return res.status(200).json(response);
       }
-      const response = await getUserDb();
+      const response = await getAllUsers();
       res.status(200).json(response);
  } catch (error) {
     res.status(400).json({error: error.message});
@@ -16,11 +16,15 @@ const getUserHandler = async (req, res) => {
   
 };
 
-const getUserIdHandler = (req, res) => {
+const getUserIdHandler = async (req, res) => {
     const { id } = req.params;
-    return res.status(200).send(`Usuario con Id:${id}`);
-    
-};    
+    try {
+        const response = await getUserById(id);
+        return res.status(200).send(response);
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+    };    
 
 module.exports = {
     getUserHandler,
